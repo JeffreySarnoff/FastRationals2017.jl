ffe42e75263312809d7bf1cfb39bb1276b4c5c4e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-27 02:55:14 +0000

    simplify canonical

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -18,18 +18,37 @@ import Base.Checked: add_with_overflow, sub_with_overflow, mul_with_overflow
 
 const SignedInt = Union{Int64, Int32, Int16, Int8, Int128, BigInt}
 
-# FastRationals are created with denom == abs(denom)
+"""
+    canonical(numerator::T, denominator::T) where T<:Signed
+
+Rational numbers that are finite and normatively bounded by
+the extremal magnitude of an underlying signed type have a
+canonical representation.
+
+- numerator and denominator have no common factors
+- numerator may be negative, zero or positive
+- denominator is strictly positive (d > 0)
+""" canonical
+
 function canonical(num::T, den::T) where {T<:SignedInt}
-    num = flipsign(num, den)
-    den = abs(den)
-    gcdenom = gcd(num, den)
-    if gcdenom !== one(T)
-        num = div(num, gcdenom)
-        den = div(den, gcdenom)
-    end
+    num, den = canonical_signed(num, den)
+    num, den = canonical_valued(num, den)
     return num, den
 end
 
+@inline
+function canonical_signed(num::T, den::T) where {T<:SignedInt}
+    return flipsign(num, den), abs(den)
+end
+
+@inline 
+function canonical_valued(num::T, den::T) where {T<:SignedInt}
+    gcdval = gcd(num, den)
+    gcdval === one(T) && return num, den
+    num = div(num, gcdval)
+    den = div(den, gcdval)
+    return num, den
+end    
 
 const PlainRational = NamedTuple{(:num, :den, :void)}
 
