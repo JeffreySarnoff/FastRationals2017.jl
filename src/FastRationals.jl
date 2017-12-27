723f99c343f495a0f35c44afcec81dd4780a8587
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-27 02:56:03 +0000

    simplify canonical

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -46,17 +46,37 @@ FastRational(::Type{R}, num::T, den::T) where {T<:SignedInt, R<:Reduceable} = Fa
 @inline FastRational(::Type{IsReduced}, numer::T, denom::T) where {T<:SignedInt} = FastRational{T, IsReduced}(numer, denom)
 
 
+"""
+    canonical(numerator::T, denominator::T) where T<:Signed
 
-# FastRationals are created with denom == abs(denom)
+Rational numbers that are finite and normatively bounded by
+the extremal magnitude of an underlying signed type have a
+canonical representation.
+
+- numerator and denominator have no common factors
+- numerator may be negative, zero or positive
+- denominator is strictly positive (d > 0)
+""" canonical
+
 function canonical(num::T, den::T) where {T<:SignedInt}
-    num = flipsign(num, den)
-    den = abs(den)
-    gcdivisor = gcd(den, num)
-    gcdivisor === one(T) && return FastRational{T, IsReduced}(num, den)
-    num = div(num, gcdivisor)
-    den = div(den, gcdivisor)
-    return FastRational{T, IsReduced}(num, den)
+    num, den = canonical_signed(num, den)
+    num, den = canonical_valued(num, den)
+    return num, den
 end
+
+@inline
+function canonical_signed(num::T, den::T) where {T<:SignedInt}
+    return flipsign(num, den), abs(den)
+end
+
+@inline 
+function canonical_valued(num::T, den::T) where {T<:SignedInt}
+    gcdval = gcd(num, den)
+    gcdval === one(T) && return num, den
+    num = div(num, gcdval)
+    den = div(den, gcdval)
+    return num, den
+end    
 
 # FastRationals are created with denom == abs(denom)
 function canonical(q::FastRational{T, R}) where {T<:SignedInt, R<:Reduceable}
