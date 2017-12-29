b860e3320d3e7b8de740b0d5d2a35429b0537264
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 02:12:36 +0000

    Update benchmark.jl

--- bench/benchmark.jl
+++ bench/benchmark.jl
@@ -1,1 +1,7 @@
+using FastRationals
 using BenchmarkTools
+
+function hilbert_matrix(::Type{T}, n::Int=7) where T<:Signed
+    num = one(FastRational{T})
+    return [num // (T(idx) + T(inx) - one1) for idx in 1:n, inx in 1:n]
+end
